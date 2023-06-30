3274ca4ffc75babbb6027ae5317fe5a71e7f46c4
Sergio <sergiomolinaflores1@gmail.com>
2023-06-29 21:06:48 -0400

    MainActivity Boton Contacto funcionando

--- app/src/main/java/cl/samf/sprintfinalmod4/MainActivity.java
+++ app/src/main/java/cl/samf/sprintfinalmod4/MainActivity.java
@@ -2,7 +2,9 @@ package cl.samf.sprintfinalmod4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import cl.samf.sprintfinalmod4.databinding.ActivityMainBinding;
 
@@ -15,6 +17,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+    binding.buttonContacto.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, SegundaActivity.class );
+            startActivity(intent);
+        }
+    });
 
     }
 
